e0fb6ba3dbfb86e7e1fffa81413feadc2d542bb1
tegan <tegan@teux.me>
2019-05-16 19:50:17 -0700

    Finalized links to projects. Made css a little better.

---
+++ js/interactions.js
@@ -0,0 +1,26 @@
+$(document).ready(function() {
+  // Borrowed code from: https://www.electrictoolbox.com/jquey-make-entire-table-row-clickable/
+  $('.p1').click(function() {
+    window.location.href = "https://github.com/tegan-lamoureux/optional";
+  });
+
+  $('.p2').click(function() {
+    window.location.href = "https://github.com/tegan-lamoureux/Rotate-Spectre";
+  });
+
+  $('.p3').click(function() {
+    window.location.href = "https://github.com/tegan-lamoureux/rtl_sdr-to-cfile";
+  });
+
+  $('.p4').click(function() {
+    window.location.href = "https://github.com/tegan-lamoureux/qt-creator-terminalplugin";
+  });
+
+  $('.p5').click(function() {
+    window.location.href = "https://github.com/tegan-lamoureux/organic";
+  });
+
+  $('.p6').click(function() {
+    window.location.href = "https://github.com/tegan-lamoureux/go-frontend";
+  });
+});
